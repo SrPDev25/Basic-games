d61909809f66ef593a52c7b5a9fa55cc656e8bc4
SrPollo25 <ortegachkn25@gmail.com>
2022-02-05 16:21:25 +0100

    Letters check complete

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,4 +1,3 @@
-
 package hangman;
 
 import basicvideogames.IGame;
@@ -10,7 +9,7 @@ import inputs.EntradaTextos;
  */
 public class Hangman implements IGame {
 
-    private String word; //BUSCAR PASAR A CHAR
+    private String[] word; //BUSCAR PASAR A CHAR
     private String[] progress;
     private String tries;
     private int fails;
@@ -38,12 +37,13 @@ public class Hangman implements IGame {
      * ask for a word
      */
     private void askWord() {
-        this.word= (EntradaTextos.inputString("Palabra de esta partida: "));
+        this.word = (EntradaTextos.inputString("Palabra de esta partida: ").split(""));
+        tries = "";
     }
 
     private void clearProgress() {
-        progress = new String[word.length()];
-        for (int i = 0; i < word.length(); i++) {
+        progress = new String[word.length];
+        for (int i = 0; i < word.length; i++) {
             progress[i] = " ";
         }
     }
@@ -55,14 +55,37 @@ public class Hangman implements IGame {
         String letter;
         String[] checkTry = tries.split("");
         letter = EntradaTextos.inputStringChar("Letra: ");
-        while (!checkTry(letter)) {
+        while (checkTry(letter)) {
             letter = EntradaTextos.inputStringChar("Letra: ");
         }
         checkLetter(letter);
-        tries = tries + letter;
+        this.tries = this.tries + letter;
 
     }
 
+    /**
+     * Check in last tries
+     *
+     * @param character
+     * @return if exist in tries, return true
+     */
+    private boolean checkTry(String character) {//USAR EL FOREACH
+        boolean tried = false;
+        if (this.tries.length() > 0) {
+            String[] triesArr=tries.split("");
+            int pos = 0;
+            while(pos<triesArr.length){
+                if(triesArr[pos].equalsIgnoreCase(character)){
+                    tried=true;
+                    break;
+                }
+                pos++;
+            }
+        }
+        return tried;
+    }
+    
+    
     private void printProgress() {//USAR EL FOREACH
         String pProgress = "";
         String lines = "";
@@ -75,34 +98,17 @@ public class Hangman implements IGame {
     }
 
     /**
-     * Check in last tries
-     *
-     * @param character
-     * @return if exist
-     */
-    private boolean checkTry(String character) {//USAR EL FOREACH
-        boolean exist = true;
-        String[] check = tries.split("");
-        int pos = 0;
-        while (!character.equalsIgnoreCase(check[pos])) {
-            exist = false;
-            pos++;
-
-        }
-        return exist;
-    }
-
-    /**
      * Check if the letter is correct
      *
      * @param character
      */
     private void checkLetter(String character) {//USAR EL FOREACH //USAR INDEX OF
         boolean fail = true;
-        for (int i = 0; i < word.length(); i++) {
-            if (character.equals(word.charAt(i))) {
+        for (int i = 0; i < this.word.length; i++) {
+            if (character.equals(this.word[i])) {
                 progress[i] = character;
                 fail = false;
+                break;
             }
         }
         if (fail) {
